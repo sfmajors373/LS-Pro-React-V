11069c18c71a6a7ab255bde32b5ee919a4874fad
sfmajors373 <sfmajors373@gmail.com>
2017-06-27 11:56:50 -0700

    Not quite working yet

---
+++ store/src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Home from './Home';
+import Products from './Products';
+import Product from './Product';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <div className="App-header">
+            <h2>Tiny Store</h2>
+          </div>
+          <div className='Navigation'>
+            <ul>
+              <li><Link to='/'>Home</Link></li>
+              <li><Link to='/products'>Products</Link></li>
+            </ul>
+            <Route exact path='/' component={ Home } />
+            <Route exact path='/products' component={ Products } />
+          </div>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
---
+++ store/src/Products.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Product from './Product';
+
+export default (props) => (
+  <div>
+    <h2>Products</h2>
+    <ul>
+      <li>
+        <Link> to={`${props.match.url}/product1`}>
+          Product 1
+        </Link>
+      </li>
+      <li>
+        <Link to={`${props.match.url}/product2`}>
+          Product 2
+        </Link>
+      </li>
+      <li>
+        <Link to={`{props.match.url}/product3`}>
+          Product 3
+        </Link>
+      </li>
+    </ul>
+    <Route path={`${props.match.url}/:productID`} component={Product} />
+    <Route exact path={props.match.url} render={() => (
+      <h3>Select a Product</h3>
+    )} />
+  </div>
+);
